391fc74f7ec811875b2ab341ab2c6ca3091e864f
pengdong <pengdong@fengbangleasing.com>
2018-06-06 09:27:40 +0800

    Initial Commit

---
+++ opengl-project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl-project
+//
+//  Created by fb_001 on 2018/6/6.
+//  Copyright © 2018年 ped. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
