aefdc8a0a4646cb769ce0154513902485b401546
pengdong <pengdong@fengbangleasing.com>
2018-06-06 11:18:14 +0800

    Hello Window

--- opengl-project/main.cpp
+++ opengl-project/main.cpp
@@ -6,39 +6,57 @@
 //  Copyright © 2018年 ped. All rights reserved.
 //
 
-#define GLFW_INCLUDE_GLCOREARB
 #include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
 
-int main() {
-    if (!glfwInit()) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    if (!glfwInit())
+    {
         return -1;
     }
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Mac OS X required
+#endif
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
-    if (!window) {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
-    
+    glViewport(0, 0, 800, 600);
     glfwMakeContextCurrent(window);
-    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     cout << "OpenGL Vendor:" << glGetString(GL_VENDOR) << endl;
     cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << endl;
     cout << "OpenGL Version: " << glGetString(GL_VERSION) << endl;
     cout << "GLSL Version:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
-    
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     glfwTerminate();
     return 0;
 }
